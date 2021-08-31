0b436ea53fdba6cd55b9cc28b17c00da74511827
matheus0000 <linuxmatheus@outlook.com>
2021-08-31 17:10:21 -0300

    NEW

--- Calculadora-de-viagem-en-km/Calculadora-de-Km-Para-Viajar.py
+++ Calculadora-de-viagem-en-km/Calculadora-de-Km-Para-Viajar.py
@@ -9,3 +9,6 @@ print('Valor-da-Viagen',('R$'),'=',(u1))
 
 # Ler valor em km do taxímetro se cada 1km vale 50 centavos então soma as vezes que vai ter a entrada dos 50 centavos
 # ( (R$ 0.50 ) Por (KM 1) ) ( 0.50 x 2) = ( R$ 1.0 ))
+
+# Viagen Longa de (R$ 0.50) tem disconto de (R$ 0.05 )
+# ( (R$ 0.50) - (R$ 0.05) = (R$ 0.45) )
