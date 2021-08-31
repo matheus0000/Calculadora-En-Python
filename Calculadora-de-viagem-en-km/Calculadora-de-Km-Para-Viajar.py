f74293eea121e0e75c76d5658edf472d8f1ce685
matheus0000 <linuxmatheus@outlook.com>
2021-08-31 16:32:57 -0300

    NEW

---
+++ Calculadora-de-viagem-en-km/Calculadora-de-Km-Para-Viajar.py
@@ -0,0 +1,11 @@
+a = int(input('Distansia en (km) Taxímetro: '))
+print('Valor-Digitato',('KM'),('='),(a));
+#Valor = ('R$',(0.50),'R$')
+Valor = (0.50)
+b = Valor
+print('Valor-da-Viagen',('Por'),('KM'),('R$'),'=',(b)) 
+u1 = (b*a)
+print('Valor-da-Viagen',('R$'),'=',(u1))
+
+# Ler valor em km do taxímetro se cada 1km vale 50 centavos então soma as vezes que vai ter a entrada dos 50 centavos
+# ( (R$ 0.50 ) Por (KM 1) ) ( 0.50 x 2) = ( R$ 1.0 ))
